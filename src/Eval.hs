066a98962c94edab644df002aa472562bb8028b0
Michael Baker <michaeltbaker@gmail.com>
2013-08-12 15:05:29 -0500

    Avoids variable capture in beta reductions

--- src/Eval.hs
+++ src/Eval.hs
@@ -1,6 +1,11 @@
-module Eval (beta) where
+module Eval (alpha, beta, freeVariables) where
 
 import Types
+
+alpha target replacement body@(V _) | target == body = replacement
+                                    | otherwise      = body
+alpha target replacement (L param body) = L (alpha target replacement param) (alpha target replacement body)
+alpha target replacement (A app arg)    = A (alpha target replacement app) (alpha target replacement arg)
 
 beta (A (L param body) arg) = replace param arg body
 beta application            = application
@@ -8,5 +13,18 @@ beta application            = application
 replace (V target) replacement (V body) | target == body = replacement
                                         | otherwise      = V body
 replace target replacement (A app arg) = A (replace target replacement app) (replace target replacement arg)
-replace target replacement (L param body) | target == param = L param body
-                                          | otherwise       = L param (replace target replacement body)
+replace target replacement (L param body) | target == param                            = L param body
+                                          | any (== param) (freeVariables replacement) = replace target replacement renamedLambda
+                                          | otherwise                                  = L param (replace target replacement body)
+                                          where renamedLambda = alpha param newVariable (L param body)
+                                                newVariable   = (availableVariable $ freeVariables body ++ freeVariables replacement)
+
+availableVariable takenVariables = head $ filter notTaken $ variableNames 'a' 1
+  where notTaken = not . (`elem` takenVariables)
+        variableNames 'z'    repetition = variableNames 'a' $ repetition + 1
+        variableNames letter repetition = makeVar letter repetition : variableNames (succ letter) repetition
+        makeVar letter repetition = V $ take repetition $ repeat letter
+
+freeVariables (V a)          = [V a]
+freeVariables (L param body) = filter (/= param) $ freeVariables body
+freeVariables (A app arg)    = freeVariables app ++ freeVariables arg
--- test/Test.hs
+++ test/Test.hs
@@ -2,7 +2,7 @@ import Test.Hspec (hspec, describe, it, shouldBe)
 
 import Parse       (parseLambdaUnsafe)
 import PrettyPrint (prettyPrint)
-import Eval        (beta)
+import Eval        (alpha, beta, freeVariables)
 import Types
 
 main = hspec $ do
@@ -25,7 +25,7 @@ main = hspec $ do
 
     describe "beta" $ do
       it "replaces the parameter of a lambda with the argument" $ do
-        "((λ a a) b)" `reducesTo` "b"
+        "((λ a a) b)"       `reducesTo` "b"
         "((λ a a) (λ b b))" `reducesTo` "(λ b b)"
 
       it "doesn't try to apply beta reduction to variables" $ do
@@ -39,3 +39,31 @@ main = hspec $ do
 
       it "does not replace bound variables in the body of a lambda" $ do
         "((λ b (λ b (a b))) c)" `reducesTo` "(λ b (a b))"
+
+      it "avoids variable capture" $ do
+        "((λ a (λ b a)) (λ x b))"                     `reducesTo` "(λ c (λ x b))"
+        "((λ c (λ b c)) (λ x b))"                     `reducesTo` "(λ a (λ x b))"
+        "((λ a (λ b (λ c a))) (λ x c))"               `reducesTo` "(λ b (λ b (λ x c)))"
+        "((λ a (λ b (λ c a))) (λ x (b c)))"           `reducesTo` "(λ d (λ d (λ x (b c))))"
+        "((λ a (λ b (λ c (λ d a)))) (λ x (d (b c))))" `reducesTo` "(λ e (λ e (λ e (λ x (d (b c))))))"
+        "((λ a (λ b (λ c a))) (λ x (b c)))"           `reducesTo` "(λ d (λ d (λ x (b c))))"
+
+    describe "freeVariables" $ do
+      let hasFreeVariables a b = (map prettyPrint $ freeVariables $ parseLambdaUnsafe a) `shouldBe` (map prettyPrint b)
+
+      it "returns a list of unbound variables in an expression" $ do
+        "b"                 `hasFreeVariables` [V "b"]
+        "(λ a b)"           `hasFreeVariables` [V "b"]
+        "((λ a b) (λ c d))" `hasFreeVariables` [V "b", V "d"]
+
+    describe "alpha" $ do
+      let replacing   = id
+          with a b    = alpha (parseLambdaUnsafe a) (parseLambdaUnsafe b)
+          inside f e  = f (parseLambdaUnsafe e)
+          becomes a b = (prettyPrint a) `shouldBe` (prettyPrint $ parseLambdaUnsafe b)
+
+      it "changes the name of the given variable everywhere in the given expression" $ do
+        replacing "a" `with` "b" `inside` "a"                 `becomes` "b"
+        replacing "a" `with` "b" `inside` "c"                 `becomes` "c"
+        replacing "a" `with` "b" `inside` "(λ a a)"           `becomes` "(λ b b)"
+        replacing "a" `with` "b" `inside` "((λ a a) (λ b a))" `becomes` "((λ b b) (λ b b))"
